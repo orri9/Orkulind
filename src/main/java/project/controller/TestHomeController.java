139a767d7e021a337854e34edb97966a060b0288
rutep <peturda3@gmail.com>
2018-10-09 12:27:29 +0000

    update

---
+++ src/main/java/project/controller/TestHomeController.java
@@ -0,0 +1,35 @@
+package project.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import project.service.StringManipulationService;
+
+@Controller
+public class TestHomeController {
+
+    // Instance Variables
+    StringManipulationService stringService;
+
+    // Dependency Injection
+    @Autowired
+    public TestHomeController(StringManipulationService stringService) {
+        this.stringService = stringService;
+    }
+
+    // Request mapping is the path that you want to map this method to
+    // In this case, the mapping is the root "/", so when the project
+    // is running and you enter "localhost:8080" into a browser, this
+    // method is called
+    @RequestMapping(value = "/orkulind", method = RequestMethod.GET)
+    public String home(){
+
+        // The string "Index" that is returned here is the name of the view
+        // (the Index.jsp file) that is in the path /main/webapp/WEB-INF/jsp/
+        // If you change "Index" to something else, be sure you have a .jsp
+        // file that has the same name
+        return "orkulind";
+    }
+}
