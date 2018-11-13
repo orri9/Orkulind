6440c014f4a1a7a96a81a3b9df15a784a0911cd2
rutep <peturda3@gmail.com>
2018-11-13 12:30:35 +0000

    update

--- src/main/java/project/controller/PersonalController.java
+++ src/main/java/project/controller/PersonalController.java
@@ -22,7 +22,7 @@ public class PersonalController  {
     // Notice here that since the class has "/demo", this path is "/demo/page"
     @RequestMapping("/personal")
     public String demoPage(Model model){
-    	System.out.println(User.logedUser.getName());
+    	// System.out.println(User.logedUser.getName());
 
     	return "personalPage"; // this returns a .jsp file with the path /webapp/WEB-INF/jsp/demo/demo.jsp
     }
