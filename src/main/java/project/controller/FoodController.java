ab130ad0596190cd1c8ba567ef1d716c48356560
orri9 <orrilevi@gmail.com>
2018-11-15 10:16:57 +0000

    gerdi jsp fyrir foods og lagadi food controller

--- src/main/java/project/controller/FoodController.java
+++ src/main/java/project/controller/FoodController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,22 +19,38 @@ public class FoodController {
     public FoodController(FoodService foodService) {
         this.foodService = foodService;
     }
-
-    @RequestMapping("/food")
-    public String getFoodView(Model model){
-    	model.addAttribute("storeFood", new Food());
-        return "Food";
-    }
     
-    @RequestMapping(value = "/food", method = RequestMethod.POST)
-    public String foodViewPost(@ModelAttribute("food") Food food,
+    
+    @RequestMapping(value = "/Foods", method = RequestMethod.GET)
+    public String foodViewGet(Model model){
+
+        model.addAttribute("Food",new Food());
+        
+        model.addAttribute("Foods",foodService.findAllReverseOrder());
+
+        return "Foods";
+    }
+   
+    @RequestMapping(value = "/postfood/{name}", method = RequestMethod.GET)
+    public String getFoodFromName(@PathVariable String name,
+            						Model model){
+ 
+    	model.addAttribute("Foods", foodService.findByName(name));
+    	 
+         model.addAttribute("Food", new Food());
+
+         return "Foods";
+    }
+  
+    @RequestMapping(value = "/postfood", method = RequestMethod.POST)
+    public String foodViewPost(@ModelAttribute("Food") Food food,
     							Model model) {
     	foodService.save(food);
+
+    	model.addAttribute("Foods", foodService.findAllReverseOrder());
     	
-    	model.addAttribute("food", new Food());
-    	
-    	model.addAttribute("food", foodService.findAllReverseOrder());
-    	
-    	return "food/Food";
+    	model.addAttribute("Food", new Food());
+    	    	
+    	return "Foods";
     }
 }
